d454799b3681fbe0fdb302268c5c67d03557fe2c
Ryan James <rejames@gmail.com>
2018-07-25 22:28:31 +0100

    removed action logger

--- src/store.js
+++ src/store.js
@@ -2,12 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import reduxPromise from 'redux-promise'
 import reducer from './reducers'
 
-const logger = () => next => action => {
-  console.log("logged:", action)
-  return next(action)
-}
-
-const middleware = [logger, reduxPromise]
+const middleware = [reduxPromise]
 const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
 
 export default createStoreWithMiddleware(reducer)
